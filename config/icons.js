0b4c6957190151e113841ca85801bc5d0a56ea3d
Tyler Bratton <tylerbratton96@gmail.com>
2023-02-04 11:21:12 -0500

    use fontawesome

---
+++ config/icons.js
@@ -0,0 +1,11 @@
+module.exports = function () {
+  return {
+    'free-solid-svg-icons': [
+      'arrows-rotate',
+      'bars',
+      'caret-up',
+      'magnifying-glass',
+      'xmark',
+    ],
+  };
+};
