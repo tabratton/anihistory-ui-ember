33acca5d35a1e0f9045ea8adc9258fa92072ac73
Tyler Bratton <tylerbratton96@gmail.com>
2024-02-11 19:57:21 -0500

    update ember-cli-deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -3,43 +3,34 @@
 
 module.exports = function (deployTarget) {
   const ENV = {
-    build: {},
+    build: {
+      environment: 'production',
+    },
     pipeline: {
-      // This setting runs the ember-cli-deploy activation hooks on every deploy
-      // which is necessary in order to run ember-cli-deploy-cloudfront.
-      // To disable CloudFront invalidation, remove this setting or change it to `false`.
-      // To disable ember-cli-deploy-cloudfront for only a particular environment, add
-      // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
       activateOnDeploy: true,
     },
-    s3: {
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
-      filePattern: '*',
+    'revision-data': {
+      type: 'version-commit',
     },
-    's3-index': {
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
-      allowOverwrite: true,
-    },
-    cloudfront: {
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
-    },
+    s3: {},
+    's3-index': {},
   };
 
   if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    ENV.s3.bucket = 'anihistory.moe';
-    ENV.s3.region = 'us-east-1';
-    ENV['s3-index'].bucket = 'anihistory.moe';
-    ENV['s3-index'].region = 'us-east-1';
-    ENV.cloudfront.distribution = 'E3RJ815PE513Z4';
-    ENV.cloudfront.region = 'us-east-1';
+    ENV.s3.accessKeyId = process.env.AWS_KEY;
+    ENV.s3.secretAccessKey = process.env.AWS_SECRET;
+    ENV.s3.bucket = process.env.BUCKET;
+    ENV.s3.region = process.env.REGION;
+    ENV['s3-index'].accessKeyId = process.env.AWS_KEY;
+    ENV['s3-index'].secretAccessKey = process.env.AWS_SECRET;
+    ENV['s3-index'].bucket = process.env.BUCKET;
+    ENV['s3-index'].region = process.env.REGION;
+    ENV.cloudfront = {
+      accessKeyId: process.env.AWS_KEY,
+      secretAccessKey: process.env.AWS_SECRET,
+      distribution: process.env.CLOUDFRONT_DISTRIBUTION,
+    };
   }
 
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
   return ENV;
 };
