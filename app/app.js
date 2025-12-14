11da6ff98414afa6a0fb9433e1654bea3b9ff663
Tyler Bratton <tylerbratton96@gmail.com>
2025-12-14 12:32:32 -0500

    ember 6.9.0

--- app/app.js
+++ app/app.js
@@ -1,21 +1,30 @@
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import compatModules from '@embroider/virtual/compat-modules';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
+
+import config from './config/environment';
+
+import { setConfig } from 'ember-basic-dropdown/config';
+
+setConfig({
+  rootElement: config.APP.rootElement,
+});
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
 }
 
 import './app.css';
-import compatModules from '@embroider/virtual/compat-modules';
 import './fontawesome';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
+  inspector = setupInspector(this);
 }
 
 loadInitializers(App, config.modulePrefix, compatModules);
